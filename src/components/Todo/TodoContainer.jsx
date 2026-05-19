f4fcdbd570ce6bdfffff71ab58fd3a9546525ef0
k25harikesh <k25harikesh.s@gmail.com>
2026-05-19 22:26:29 +0530

    add input data to tasklist

--- src/components/Todo/TodoContainer.jsx
+++ src/components/Todo/TodoContainer.jsx
@@ -1,49 +1,103 @@
+import { useState } from 'react'
 import './TodoContainer.css'
 
+
+function HeaderCard({total, completed}){
+  return (
+      <div className='header-card'>
+        <div className="header-info">
+          <h1>Todo Done</h1>
+          <p>Keep it up</p>
+        </div>
+        <div className="progress-circle">
+          <span>{completed} / {total}</span>
+        </div>
+      </div>
+  )
+}
+
+function InputRow({inputValue, setInputValue, addTask}){
+
+  function handleSubmit(e){
+    e.preventDefault()
+    addTask()    
+  }
+
+  return (
+      <form className='input-row' onSubmit={handleSubmit}>
+        <input
+         type="text" name='task-input' placeholder='Write Next Task' className='task-input'
+         value={inputValue} onChange={(e) => {setInputValue(e.target.value)}}/>
+
+        <button type='submit' name='add-btn' className='add-btn'>+</button>
+      </form>
+  )
+}
+
+function TaskList({tasks}){
+  return (
+      <div className="task-list">
+        {tasks.map((task, index) => {
+          return (
+            <div className="task-item" key={index}>
+              <div className="task-left">
+                <span className="circle-icon alert"></span>
+                <span className="task-text">{task}</span>
+              </div>
+              <div className="task-actions">
+                <button className="action-btn">🖊️</button>
+                <button className="action-btn">🍴</button>
+              </div>
+            </div>
+          )
+          })
+        }
+        {/* Active Task */}
+        {/* <div className="task-item">
+          <div className="task-left">
+            <span className="circle-icon alert"></span>
+            <span className="task-text">Task 1</span>
+          </div>
+          <div className="task-actions">
+            <button className="action-btn">🖊️</button>
+            <button className="action-btn">🍴</button>
+          </div>
+        </div> */}
+        {/* completed task */}
+        {/* <div className="task-item completed">
+          <div className="task-left">
+            <span className="circle-icon success"></span>
+            <span className="task-text">Task 2</span>
+          </div>
+          <div className="task-actions">
+            <button className="action-btn">🖊️</button>
+            <button className="action-btn">🍴</button>
+          </div>
+        </div>           */}
+      </div>
+  )
+}
+
 export default function TodoContainer(){
+
+  const [inputValue, setInputValue] = useState("")
+  const [tasks, setTasks] = useState([])
+
+  function addTask(){
+    if(inputValue.trim() === '') return
+    setTasks([...tasks, inputValue])      
+    setInputValue('')
+  }
+
+  const total = tasks.length
+  const completed = 1
+
+
   return (
       <div className='todo-container'>
-
-        <div className='header-card'>
-          <div className="header-info">
-            <h1>Todo Done</h1>
-            <p>Keep it up</p>
-          </div>
-          <div className="progress-circle">
-            <span>1 / 3</span>
-          </div>
-        </div>
-
-        <form className='input-row'>
-          <input type="text" name='task-input' placeholder='Write Next Task' className='task-input'/>
-          <button type='submit' name='add-btn' className='add-btn'>+</button>
-        </form>
-
-        <div className="task-list">
-          {/* Active Task */}
-          <div className="task-item">
-            <div className="task-left">
-              <span className="circle-icon alert"></span>
-              <span className="task-text">Task 1</span>
-            </div>
-            <div className="task-actions">
-              <button className="action-btn">🖊️</button>
-              <button className="action-btn">🍴</button>
-            </div>
-          </div>
-          {/* completed task */}
-          <div className="task-item completed">
-            <div className="task-left">
-              <span className="circle-icon success"></span>
-              <span className="task-text">Task 2</span>
-            </div>
-            <div className="task-actions">
-              <button className="action-btn">🖊️</button>
-              <button className="action-btn">🍴</button>
-            </div>
-          </div>          
-        </div>
-
+        <HeaderCard total={total} completed={completed} />
+        <InputRow inputValue={inputValue} setInputValue={setInputValue} addTask={addTask} />        
+        <TaskList tasks={tasks} />
       </div>
   )
 }
