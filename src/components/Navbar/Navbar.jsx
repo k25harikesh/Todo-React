c7c7660620eb6fb62e99e0cbe17fd8789dbf8d19
k25harikesh <k25harikesh.s@gmail.com>
2026-05-19 09:09:19 +0530

    Use components to modularize the whole repo

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import './Navbar.css'
+
+
+export default function Navbar(){
+  return (
+      <nav>
+        <h1>XERO<span style={{color: 'tomato'}}>Todo</span></h1>      
+      </nav>
+  )
+}
---
+++ src/components/Todo/TodoContainer.jsx
@@ -0,0 +1,49 @@
+import './TodoContainer.css'
+
+export default function TodoContainer(){
+  return (
+      <div className='todo-container'>
+
+        <div className='header-card'>
+          <div className="header-info">
+            <h1>Todo Done</h1>
+            <p>Keep it up</p>
+          </div>
+          <div className="progress-circle">
+            <span>1 / 3</span>
+          </div>
+        </div>
+
+        <form className='input-row'>
+          <input type="text" name='task-input' placeholder='Write Next Task' className='task-input'/>
+          <button type='submit' name='add-btn' className='add-btn'>+</button>
+        </form>
+
+        <div className="task-list">
+          {/* Active Task */}
+          <div className="task-item">
+            <div className="task-left">
+              <span className="circle-icon alert"></span>
+              <span className="task-text">Task 1</span>
+            </div>
+            <div className="task-actions">
+              <button className="action-btn">🖊️</button>
+              <button className="action-btn">🍴</button>
+            </div>
+          </div>
+          {/* completed task */}
+          <div className="task-item completed">
+            <div className="task-left">
+              <span className="circle-icon success"></span>
+              <span className="task-text">Task 2</span>
+            </div>
+            <div className="task-actions">
+              <button className="action-btn">🖊️</button>
+              <button className="action-btn">🍴</button>
+            </div>
+          </div>          
+        </div>
+
+      </div>
+  )
+}
